bb02c98af26548712b7599f4d65aa2ff5f2fca36
Shlomi Matichin <shlomi@capester.com>
2016-09-11 17:56:29 +0300

    Added --replaceWith to removefunctioncalls

--- py/removefunctioncalls.py
+++ py/removefunctioncalls.py
@@ -3,6 +3,7 @@ import codeprocessingtokens
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--functionName", nargs="+", default=["NSLog"])
+parser.add_argument("--replaceWith", default="")
 parser.add_argument("--replaceInline", action="store_true")
 parser.add_argument("filename", nargs="+")
 args = parser.parse_args()
@@ -18,8 +19,10 @@ for filename in args.filename:
                 closingParen = tokens.closingParen(tokens[token.index + 1])
                 semicolon = tokens[closingParen.index + 1]
                 assert semicolon.spelling == ';'
-                space = " " * (semicolon.offset + 1 - token.offset)
-                content = content[:token.offset] + space + content[semicolon.offset + 1:]
+                size = semicolon.offset + 1 - token.offset
+                assert size >= len(args.replaceWith)
+                replace = args.replaceWith + " " * (size - len(args.replaceWith))
+                content = content[:token.offset] + replace + content[semicolon.offset + 1:]
     if args.replaceInline:
         with open(filename, "w") as f:
             f.write(content)
