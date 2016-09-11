5423cf12a2e3a30133ac6079efeeaedbc30c2486
Shlomi Matichin <shlomi@capester.com>
2016-09-11 16:43:06 +0300

    removefunctioncalls supports multiple function names

--- py/removefunctioncalls.py
+++ py/removefunctioncalls.py
@@ -2,7 +2,7 @@ import argparse
 import codeprocessingtokens
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--functionName", default="NSLog")
+parser.add_argument("--functionName", nargs="+", default=["NSLog"])
 parser.add_argument("--replaceInline", action="store_true")
 parser.add_argument("filename", nargs="+")
 args = parser.parse_args()
@@ -11,14 +11,15 @@ for filename in args.filename:
     with open(filename) as f:
         content = f.read()
     tokens = codeprocessingtokens.Tokens.fromFile(filename)
-    for token in tokens:
-        if token.spelling == args.functionName:
-            assert tokens[token.index + 1].spelling == "("
-            closingParen = tokens.closingParen(tokens[token.index + 1])
-            semicolon = tokens[closingParen.index + 1]
-            assert semicolon.spelling == ';'
-            space = " " * (semicolon.offset + 1 - token.offset)
-            content = content[:token.offset] + space + content[semicolon.offset + 1:]
+    for functionName in args.functionName:
+        for token in tokens:
+            if token.spelling == functionName:
+                assert tokens[token.index + 1].spelling == "("
+                closingParen = tokens.closingParen(tokens[token.index + 1])
+                semicolon = tokens[closingParen.index + 1]
+                assert semicolon.spelling == ';'
+                space = " " * (semicolon.offset + 1 - token.offset)
+                content = content[:token.offset] + space + content[semicolon.offset + 1:]
     if args.replaceInline:
         with open(filename, "w") as f:
             f.write(content)
